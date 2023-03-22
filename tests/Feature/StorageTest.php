b5b20bafa0bd2218048857b06d2e34dea717d098
DedeSandi <gdekasandiyasa@gmail.com>
2023-03-22 16:54:00 +0800

    file storage

---
+++ tests/Feature/StorageTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Storage;
+use Tests\TestCase;
+
+class StorageTest extends TestCase
+{
+    public function testStorage()
+    {
+        $fileSystem = Storage::disk('local');
+        $fileSystem->put('file.txt', 'Hello World');
+        $content = $fileSystem->get('file.txt');
+
+        self::assertEquals('Hello World', $content);
+    }
+    public function testPublic()
+    {
+        $fileSystem = Storage::disk('public');
+        $fileSystem->put('file.txt', 'Hello World');
+        $content = $fileSystem->get('file.txt');
+
+        self::assertEquals('Hello World', $content);
+    }
+}
